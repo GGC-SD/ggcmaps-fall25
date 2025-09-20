eb8e6e9ef31b218737221b5b41b2277e3cc27594
William Stein <thewillstein@gmail.com>
2025-09-20 15:32:02 -0400

    9/20 repo

---
+++ .next/static/webpack/app/building/[buildingId]/[floorId]/page.ccefe70fe5ea6605.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/building/[buildingId]/[floorId]/page",{
+
+/***/ "(app-pages-browser)/./data/buildings.json":
+/*!*****************************!*\
+  !*** ./data/buildings.json ***!
+  \*****************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = /*#__PURE__*/JSON.parse('[{"id":"A","name":"Building A","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/A/L1.svg"}]},{"id":"B","name":"Building B","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/B/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/B/L2.svg"},{"id":"L3","label":"Level 3","file":"/BuildingMaps/B/L3.svg"}]},{"id":"C","name":"Building C","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/C/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/C/L2.svg"}]},{"id":"D","name":"Building D / Admissions","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/D/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/D/L2.svg"}]},{"id":"E","name":"Building E / Student Center","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/E/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/E/L2.svg"},{"id":"L3","label":"Level 3","file":"/BuildingMaps/E/L3.svg"}]},{"id":"F","name":"Building F / Wellness & Rec Center","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/F/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/F/L2.svg"}]},{"id":"H","name":"Building H / Allied Health & Science","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/H/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/H/L2.svg"},{"id":"L3","label":"Level 3","file":"/BuildingMaps/H/L3.svg"}]},{"id":"I","name":"Building I","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/I/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/I/L2.svg"},{"id":"L3","label":"Level 3","file":"/BuildingMaps/I/L3.svg"}]},{"id":"L","name":"Building L / Library","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/L/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/L/L2.svg"},{"id":"L3","label":"Level 3","file":"/BuildingMaps/L/L3.svg"}]},{"id":"W","name":"Building W","floors":[{"id":"GL","label":"Ground Level","file":"/BuildingMaps/W/GL.svg"},{"id":"L1","label":"Level 1","file":"/BuildingMaps/W/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/W/L2.svg"}]},{"id":"2000","name":"Building 2000","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/2000/L1.svg"}]},{"id":"3000","name":"Building 3000","floors":[{"id":"L1","label":"Level 1","file":"/BuildingMaps/3000/L1.svg"},{"id":"L2","label":"Level 2","file":"/BuildingMaps/3000/L2.svg"},{"id":"L3","label":"Level 3","file":"/BuildingMaps/3000/L3.svg"},{"id":"L4","label":"Level 4","file":"/BuildingMaps/3000/L4.svg"}]}]');
+
+/***/ })
+
+});
